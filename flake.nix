cac6c33552426256b7610d1a5daf8d6998cb561e
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-12-26 11:56:34 +0100

    Add a Nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-basic enumitem;
+        };
+      in
+      {
+        defaultPackage = pkgs.mkShell {
+          buildInputs = with pkgs; [ tex python3 ];
+        };
+      }
+    );
+}
