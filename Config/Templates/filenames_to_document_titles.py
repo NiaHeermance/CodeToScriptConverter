17d868dd0ae44dafabb9a62abd1944d544e3c824
NiaHeermance <niaheermance@gmail.com>
2023-12-26 05:24:18 -0500

    Update template files for working with renpy games

---
+++ Config/Templates/filenames_to_document_titles.py
@@ -0,0 +1,19 @@
+# _,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'
+#
+# filenames_to_document_titles.py
+# Nia~
+#
+# Make sure to fill out get_document_title() before using CodeToScriptConverter.
+# Copy finished version to Config/Files.
+#
+# _,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'
+
+def get_document_title(file_name: str) -> str:
+    """
+    Given a file_name, returns an appropiate title for a document script based
+    on that file.
+
+    file_name -- Name of a file, not including extension
+    return: Appropiate title for script docuemnt
+    """
+   pass
