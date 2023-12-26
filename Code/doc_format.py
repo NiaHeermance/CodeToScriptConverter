618c665f795632f3ec9b95491c498a71f45f9594
NiaHeermance <niaheermance@gmail.com>
2023-12-25 22:43:46 -0500

    Add capacity to render .tex into PDFs

--- Code/doc_format.py
+++ Code/doc_format.py
@@ -1,4 +1,14 @@
 import sys
+import os
+
+# _,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'
+#
+# doc_format.py
+# Nia~
+#
+# These functions can be used to create simple LaTeX documents with lists and stylized text.
+#
+# _,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'
 
 def bold(text: str) -> str:
 	return f'\\textbf{{{text}}}'
@@ -7,19 +17,10 @@ def italics(text: str) -> str:
 	return f'\\textit{{{text}}}'
 
 def underline(text: str) -> str:
-		return f'\\underline{{{text}}}'
+	return f'\\underline{{{text}}}'
 
 def quote(text: str) -> str:
 		return f"``{text}''"
-
-def list_item(text: str) -> str:
-	return f'\\item {text}\n'
-
-def start_list(bullet_type: str) -> str:
-	return f'\\begin{{enumerate}}[{bullet_type}]\n'
-
-def end_list() -> str:
-	return "\\end{enumerate}\n"
 
 def get_text_with_style(text: str, style: str) -> str:
 	"""
@@ -35,9 +36,40 @@ def get_text_with_style(text: str, style: str) -> str:
 	style_func = getattr(thismodule, style)
 	return style_func(text)
 
+
+def start_list(bullet_type: str) -> str:
+	label = "label = "
+	if bullet_type == "number":
+		label += "\\arabic*."
+	elif bullet_type == "letter":
+		label += "\\alph*."
+
+	return f'\\begin{{enumerate}}[{label}]\n'
+
+def list_item(text: str) -> str:
+	return f'\\item {text}\n'
+
+def end_list() -> str:
+	return "\\end{enumerate}\n"
+
+def new_line() -> str:
+	return "\\\\"
+
+
 def document_format_start():
 	with open("../Code/latex_preamble.txt", "r") as preamble:
 		return preamble.read()
 
 def document_format_end():
 	return "\\end{document}"
+
+
+def renderDocument(file_path: str):
+	"""
+	Given a file_path, renders the tex file into a PDF
+	file_path -- absolute or relative path to a .tex file.
+	"""
+	final_slash = file_path.rindex("/")
+	output_directory = file_path[:final_slash]
+	aux_directory = output_directory + "/AuxiliaryFiles"
+	os.system(f'pdflatex {file_path} --output-directory={output_directory} --aux-directory={aux_directory}')
--- Code/script_writer.py
+++ Code/script_writer.py
@@ -1,10 +1,27 @@
+# _,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'
+#
+# script_writer.py
+# Nia~
+#
+# This class ScriptWriter can create PDF scripts with an easy API.
+#
+# _,.-'~'-.,__,.-'~'-.,__,.-'~'-.,__,.-'~'
+
 from io import StringIO
-from doc_format import *
+import doc_format
 
 class ScriptWriter:
-	def __init__(self, file_name):
+	# ----
+	# Member Functions
+	# ----
+
+	def __init__(self, file_name: str):
+		"""
+		Sets up a ScriptWriter object to be able to add lines to a document.
+
+		file_name - The name of the file we are creating.
+		"""
 		self.in_sublist = False
-		
 		self.scene_count = 1
 
 		self.scene_characters = set()
@@ -12,42 +29,32 @@ class ScriptWriter:
 
 		self.scene_buffer = StringIO()
 		self.document_buffer = StringIO()
-		self.output_file = open(f'../Output/{file_name}.tex', "w")
+		self.output_file_path = f'../Output/{file_name}.tex'
+		self.output_file = open(self.output_file_path, "w")
 
 
-	def writeLine(self, character, text, style):
-		line = f'{character}: {get_text_with_style(text, style)}'
-		self.scene_buffer.write(list_item(line))
-
-		self.scene_characters.add(character)
-
-
-	def beginPathExclusiveLines(self):
+	def addLine(self, character: str, text: str, style: str):
+		"""
+		Adds a line to the script.
+		character -- Name of character saying the line.
+		text      -- Text in line
+		style     -- Way line is written. "bold," "italic," "underline," and "quote."
+		"""
 		if self.in_sublist:
-			self.endPathExclusiveLines()
-
-		self.scene_buffer.write(list_item(""))
-		self.scene_buffer.write(start_list("a."))
-		self.in_sublist = True
+			self.end_path_exclusive_lines()
+		self.write_line(character, text, style)
 
 
-	def endPathExclusiveLines(self):
-		self.scene_buffer.write(end_list())
-		self.in_sublist = False
-
-
-	def pathExclusiveLine(self, character, text, style):
+	def pathExclusiveLine(self, character: str, text: str, style: str):
+		"""
+		Adds a path-exclusive line to the script.
+		character -- Name of character saying the line.
+		text      -- Text in line
+		style     -- Way line is written. "bold," "italic," "underline," and "quote."
+		"""
 		if not self.in_sublist:
-			self.beginPathExclusiveLines()
-
-		self.writeLine(character, text, style)
-
-
-	def addLine(self, character, text, style):
-		if self.in_sublist:
-			self.endPathExclusiveLines()
-
-		self.writeLine(character, text, style)
+			self.begin_path_exclusive_lines()
+		self.write_line(character, text, style)
 
 
 	def writeCharacterList(self, io_buffer, characters):
@@ -61,24 +68,37 @@ class ScriptWriter:
 				io_buffer.write(f'{character}\n\n')
 
 
-	def beginDocument(self, name):
-		self.output_file.write(document_format_start())
-		self.output_file.write(bold(name) + "\n")
+	def beginDocument(self, name: str):
+		""" 
+		Begins a document with title "name."
+
+		name -- Title of document, not the fle name.
+		"""
+		self.output_file.write(doc_format.document_format_start())
+		self.output_file.write(doc_format.bold(name) + "\n\n")
 
 
-	def beginScene(self, scene_name):
+	def beginScene(self, scene_name: str):
+		"""
+		Begins a scene with name "scene_name."
+
+		scene_name -- Name of the scene used in header.
+		"""
 		self.scene_buffer = StringIO()
 		self.scene_characters.clear()
 
-		text = bold(f'Scene {self.scene_count}, {scene_name}')
+		text = doc_format.bold(f'Scene {self.scene_count}, {scene_name}')
 		self.scene_count += 1
-		self.document_buffer.write(f'\n{text}\n')
+		self.document_buffer.write(f'\n{text}\n\n')
 		
-		self.scene_buffer.write(start_list("1."))
+		self.scene_buffer.write(doc_format.start_list("number"))
 
 
 	def endScene(self):
-		self.scene_buffer.write(end_list())
+		"""
+		Does the necessary formatting to end a scene.
+		"""
+		self.scene_buffer.write(doc_format.end_list())
 
 		self.writeCharacterList(self.document_buffer, self.scene_characters)
 		self.scene_buffer.seek(0)
@@ -89,10 +109,53 @@ class ScriptWriter:
 
 
 	def endDocument(self):
+		"""
+		Pushes the buffer to the document and saves the file.
+		"""
 		self.writeCharacterList(self.output_file, self.document_characters)
 		self.document_buffer.seek(0)
 		self.output_file.write(self.document_buffer.read())
-		self.output_file.write(document_format_end())
+		self.output_file.write(doc_format.document_format_end())
 
 		self.document_buffer.close()
 		self.output_file.close()
+
+
+	def renderDocument(self):
+		"""
+		Renders the document into a PDF if not already so.
+		Should only be called after endDocument is called.
+		"""
+		doc_format.renderDocument(self.output_file_path)
+
+
+	# ---------------------------------
+
+	# ----
+	# Private Functions
+	# ----
+
+	# Adds a line to the script. Called by other functions.
+	# character -- Name of character saying the line.
+	# text      -- Text in line
+	# style     -- Way line is written. "bold," "italic," "underline," and "quote."
+	def write_line(self, character: str, text: str, style: str):
+		line = f'{character}: {doc_format.get_text_with_style(text, style)}'
+		self.scene_buffer.write(doc_format.list_item(line))
+		self.scene_characters.add(character)
+
+
+	# Sets up the document for adding path-exclusive lines.
+	def begin_path_exclusive_lines(self):
+		if self.in_sublist:
+			self.end_path_exclusive_lines()
+
+		self.scene_buffer.write(doc_format.list_item(""))
+		self.scene_buffer.write(doc_format.start_list("letter"))
+		self.in_sublist = True
+
+
+	# Ends a block of path-exclusive lines.
+	def end_path_exclusive_lines(self):
+		self.scene_buffer.write(doc_format.end_list())
+		self.in_sublist = False
